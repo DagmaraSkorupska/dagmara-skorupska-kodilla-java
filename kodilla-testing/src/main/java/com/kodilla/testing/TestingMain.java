8e7978e4e19951effbfe45516dc74b1c2c326edc
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-06-10 17:56:06 +0200

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("Daga");
+
+        String reslut = simpleUser.getUsername();
+
+        if(reslut.equals("Daga")){
+            System.out.println("test OK");
+        } else{
+            System.out.println("Error!");
+        }
+        //zadanie
+        System.out.println("Test- pierwszy test jednostkowy");
+
+        Calculator calculator = new Calculator();
+
+        System.out.println("a+b= " + calculator.addAToB(5,2));
+        System.out.println("a-b= " + calculator.subtrackAToB(5,2));
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int a,b;
+
+
+    public int addAToB (int a, int b){
+        return a+b;
+    }
+    public int subtrackAToB(int a, int b){
+        return a-b;
+    }
+}
