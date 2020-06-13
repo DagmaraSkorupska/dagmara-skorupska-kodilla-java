3f291f415ffc8c74a70068b55066f8383117dea4
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-06-13 21:32:14 +0200

    fix Testing Calculator class

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,21 +5,13 @@ import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String reslut = simpleUser.getUsername();
-
-        if(reslut.equals("theForumUser")){
-            System.out.println("test OK");
-        } else{
-            System.out.println("Error!");
-        }
         //Testing Calculator class
         System.out.println("Test- pierwszy test jednostkowy");
 
         Calculator calculator = new Calculator();
 
         System.out.println("a+b= " + calculator.addAToB(5,2));
-        System.out.println("a-b= " + calculator.subtrackBFromA(5,2));
+        System.out.println("a-b= " + calculator.subtractBFromA(5,2));
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -7,7 +7,7 @@ public class Calculator {
     public int addAToB (int a, int b){
         return a+b;
     }
-    public int subtrackBFromA(int a, int b){
+    public int subtractBFromA(int a, int b){
         return a-b;
     }
 }
