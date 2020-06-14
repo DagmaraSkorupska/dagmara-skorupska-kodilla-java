94d3d5423d1f70edeb5f398240471046027af52d
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-06-14 16:09:44 +0200

    zad 6.3 Test klasy wybierającej parzyste liczby z kolekcji

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -2,16 +2,20 @@ package com.kodilla.testing;
 
 import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.collection.OddNumbersExterminator;
 
 public class TestingMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //Testing Calculator class
         System.out.println("Test- pierwszy test jednostkowy");
 
         Calculator calculator = new Calculator();
 
-        System.out.println("a+b= " + calculator.addAToB(5,2));
-        System.out.println("a-b= " + calculator.subtractBFromA(5,2));
+        System.out.println("a+b= " + calculator.addAToB(5, 2));
+        System.out.println("a-b= " + calculator.subtractBFromA(5, 2));
+
+
+
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,45 @@
+package com.kodilla.testing.collection;
+
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.*;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class CollectionTestSuite {
+
+    @Before
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+        //When
+        ArrayList<Integer> listNumbers = OddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Testing " + listNumbers);
+        //Then
+        Assert.assertEquals(emptyList,listNumbers);
+    }
+
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        //Given
+        ArrayList<Integer> normalList = new ArrayList<Integer>();
+        ArrayList<Integer> oddList = new ArrayList<Integer>();
+
+        //When
+        ArrayList<Integer> listNumbers = OddNumbersExterminator.exterminate(normalList);
+        System.out.println("Testing " + listNumbers);
+        //Then
+        Assert.assertEquals(oddList, listNumbers);
+    }
+
+
+}
