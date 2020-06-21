80e2828588c2e2b020149c9e6218445b8f1af1df
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-06-21 21:45:55 +0200

    Task 6.6 JUnit + Mockito

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,44 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BookLibrary {
+    LibraryDatabase libraryDatabase;
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3)
+            return bookList;
+
+        List<Book> resultList = libraryDatabase.listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20)
+            return bookList;
+
+        bookList = resultList;
+
+        return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser){
+        List<Book> bookListInHands = libraryDatabase.listBooksInHandsOf(libraryUser);
+
+        if(bookListInHands.size() == 0) {
+            System.out.println("Użytkownik ma wypozyczonych " + bookListInHands.size() + " ksiazek");
+        }
+
+        if(bookListInHands.size() == 1)
+            return bookListInHands;
+
+        if(bookListInHands.size() == 5)
+            return bookListInHands;
+
+        return bookListInHands;
+    }
+
+}
