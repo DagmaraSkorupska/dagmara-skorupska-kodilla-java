559edc0399afe2b006593f75fe0d20ea3b970a2b
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-07-01 22:08:24 +0200

    Task 8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FindFlight.java
@@ -0,0 +1,21 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+
+public class FindFlight {
+    public void findFlight(Flight flight) throws RouteNotFoundException{
+        HashMap<String, Boolean> airport = new HashMap<>();
+        airport.put("Paris", true);
+        airport.put("Warsaw", true);
+        airport.put("New York", true);
+        airport.put("Moscow", true);
+        airport.put("London", false);
+        airport.put("Poznan", false);
+
+        if(airport.containsKey(flight.getArrivalAirport()) == true){
+            System.out.println("From this airport travel to: " + flight.getArrivalAirport());
+        } else {
+            throw new RouteNotFoundException("Arrival airport doesn't exist");
+        }
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/Main.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.test;
+
+public class Main {
+    public static void main(String[] args){
+        Flight flight = new Flight("Roma", "Barcelona");
+        FindFlight flightFinder = new FindFlight();
+
+        try{
+            flightFinder.findFlight(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println("Error: " + e);
+        }
+
+    }
+}
