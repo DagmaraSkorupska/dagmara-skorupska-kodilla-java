85f149e92f6fc06c023e9472950b437457e3c3f8
DagmaraSkorupska <66071099+DagmaraSkorupska@users.noreply.github.com>
2020-09-29 11:40:49 +0200

    Task 16.4 -update2

--- MySQL task 16.4.sql
+++ MySQL task 16.4.sql
@@ -1,11 +1,11 @@
-select issues.SUMMARY as Nazwa_zadania, issueslists.NAME as Lista_zadan
+select issues.SUMMARY as task_name, issueslists.NAME as List_task
 from issues join issueslists
 on issues.ISSUESLIST_ID = issueslists.ID;
 
-select issues.SUMMARY as Nazwa_zadania, users.ID, users.LASTNAME as Nazwisko
+select issues.SUMMARY as task_name, users.ID, users.LASTNAME
 from issues join users
 on issues.USER_ID_ASSIGNEDTO = users.id; 
 
-select users.FIRSTNAME as Imie, users.LASTNAME as Nazwisko, count(*) as Ilosc_zadan
+select users.FIRSTNAME, users.LASTNAME, count(*) as number_of_tasks
 from users join issues on issues.USER_ID_ASSIGNEDTO = users.id
 group by USER_ID_ASSIGNEDTO;
