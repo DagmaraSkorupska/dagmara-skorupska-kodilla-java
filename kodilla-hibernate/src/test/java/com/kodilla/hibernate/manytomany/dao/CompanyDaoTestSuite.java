223077cbe58a49a7187745a12871ef8eef6ecc5e
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-10-07 19:06:20 +0200

    Task 17.4

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/manytomany/dao/CompanyDaoTestSuite.java
@@ -0,0 +1,111 @@
+package com.kodilla.hibernate.manytomany.dao;
+
+import com.kodilla.hibernate.manytomany.Company;
+import com.kodilla.hibernate.manytomany.Employee;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompanyDaoTestSuite {
+    @Autowired
+    CompanyDao companyDao;
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @Test
+    public void testSaveManyToMany(){
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        softwareMachine.getEmployees().add(johnSmith);
+        dataMaesters.getEmployees().add(stephanieClarckson);
+        dataMaesters.getEmployees().add(lindaKovalsky);
+        greyMatter.getEmployees().add(johnSmith);
+        greyMatter.getEmployees().add(lindaKovalsky);
+
+        johnSmith.getCompanies().add(softwareMachine);
+        johnSmith.getCompanies().add(greyMatter);
+        stephanieClarckson.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(dataMaesters);
+        lindaKovalsky.getCompanies().add(greyMatter);
+
+        //When
+        companyDao.save(softwareMachine);
+        int softwareMachineId = softwareMachine.getId();
+
+        companyDao.save(dataMaesters);
+        int dataMaestersId = dataMaesters.getId();
+
+        companyDao.save(greyMatter);
+        int greyMatterId = greyMatter.getId();
+
+        //Then
+        Assert.assertNotEquals(0, softwareMachineId);
+        Assert.assertNotEquals(0,dataMaestersId);
+        Assert.assertNotEquals(0, greyMatterId);
+
+        //CleanUp
+
+           companyDao.delete(softwareMachine);
+           companyDao.delete(dataMaesters);
+           companyDao.delete(greyMatter);
+           companyDao.deleteAll();
+
+
+    }
+
+    @Test
+    public void testNamedQueries() {
+        //Given
+        Employee johnSmith = new Employee("John", "Smith");
+        Employee stephanieClarckson = new Employee("Stephanie", "Clarckson");
+        Employee lindaKovalsky = new Employee("Linda", "Kovalsky");
+
+        Company softwareMachine = new Company("Software Machine");
+        Company dataMaesters = new Company("Data Maesters");
+        Company greyMatter = new Company("Grey Matter");
+
+        companyDao.save(softwareMachine);
+        //int softwareMachinesId = softwareMachine.getId();
+
+        employeeDao.save(johnSmith);
+        //int johnSmithId = johnSmith.getId();
+
+        employeeDao.save(lindaKovalsky);
+        //int lindaKovalskyId = lindaKovalsky.getId();
+
+        //When
+        List<Company> searchCompany = companyDao.searchCompanyWhoFirstThreeCharactersOfName("Sof");
+        List<Employee> searchLastNameSmith = employeeDao.searchEmployeeFromLastName("Smith");
+        List<Employee> searchLastNameNowak = employeeDao.searchEmployeeFromLastName("Nowak");
+
+        //Then
+        Assert.assertEquals(1, searchCompany.size());
+        Assert.assertEquals(1, searchLastNameSmith.size());
+        Assert.assertEquals(0, searchLastNameNowak.size());
+
+        //CleanUp
+        companyDao.delete(softwareMachine);
+        employeeDao.delete(johnSmith);
+        employeeDao.delete(lindaKovalsky);
+        employeeDao.deleteAll();
+        companyDao.deleteAll();
+
+    }
+
+}
