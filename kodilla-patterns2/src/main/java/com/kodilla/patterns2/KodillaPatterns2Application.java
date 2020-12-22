35a258022f5ffb6a372b1a9c908fd65085b4ba98
DagmaraSkorupska <dagmaraskorupska@gmail.com>
2020-12-22 17:47:08 +0100

    example + task 24.3

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaPatterns2Application.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/KodillaPatterns2Application.java
@@ -3,6 +3,7 @@ package com.kodilla.patterns2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class KodillaPatterns2Application {
 
